4f85ced12de53848d82528bd752e7a8fd86bc225
PierreWeets <pierre.weets@gmail.com>
2020-09-29 09:12:24 +0200

    1st ver

---
+++ build.js
@@ -0,0 +1,69 @@
+'use strict'
+
+// Pull in our modules
+
+
+const chalk = require('chalk');
+const fs = require('fs');
+const path = require('path');
+//const boxen = require('boxen');
+
+// Define options for Boxen
+const options = {
+  padding: 1,
+  margin: 1,
+  borderStyle: 'round'
+};
+
+// Text + chalk definitions
+const data = {
+  //name: chalk.white('               Tierney Cyren'),
+  name: chalk.white('               Weets Pierre'),
+  //handle: chalk.white('bitandbang'),
+  handle: chalk.white('Piwi'),
+  //work: chalk.white('Senior Cloud Developer Advocate at Microsoft'),
+  work: chalk.white('junior web developper at Becode Charleroi'),
+  //opensource: chalk.white('Node.js Community Committee ') + chalk.green('⬢'),
+  //twitter: chalk.gray('https://twitter.com/') + chalk.cyan('bitandbang'),
+  //npm: chalk.gray('https://npmjs.com/') + chalk.red('~bnb'),
+  npm: chalk.gray('https://npmjs.com/') + chalk.red('~Piwi'),
+  //github: chalk.gray('https://github.com/') + chalk.green('bnb'),
+  github: chalk.gray('https://github.com/') + chalk.green('PierreWeets'),
+  linkedin: chalk.gray('https://linkedin.com/in/') + chalk.blue('Piwi'),
+  //web: chalk.cyan('https://bnb.im'),
+  npx: chalk.red('npx') + ' ' + chalk.white('@piwi/card  (via GitHub Package Registry)'),
+  labelWork: chalk.white.bold('       Work:'),
+  //labelOpenSource: chalk.white.bold('Open Source:'),
+  //labelTwitter: chalk.white.bold('    Twitter:'),
+  labelnpm: chalk.white.bold('        npm:'),
+  labelGitHub: chalk.white.bold('     GitHub:'),
+  labelLinkedIn: chalk.white.bold('   LinkedIn:'),
+  //labelWeb: chalk.white.bold('        Web:'),
+  labelCard: chalk.white.bold('       Card:')
+};
+
+// Actual strings we're going to output
+const newline = '\n';
+const heading = `${data.name} / ${data.handle}`;
+const working = `${data.labelWork}  ${data.work}`;
+//const opensourcing = `${data.labelOpenSource}  ${data.opensource}`;
+//const twittering = `${data.labelTwitter}  ${data.twitter}`;
+//const npming = `${data.labelnpm}  ${data.npmhandlea.github}`;
+const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`;
+//const webing = `${data.labelWeb}  ${data.web}`;
+const carding = `${data.labelCard}  ${data.npx}`;
+
+// Put all our output together into a single variable so we can use boxen effectively
+const output = heading + // data.name + data.handle
+               newline + newline + // Add one whole blank line
+               working + newline + // data.labelWork + data.work
+               //opensourcing + newline + newline + // data.labelOpenSource + data.opensource
+               //twittering + newline + // data.labelTwitter + data.twitter
+               //npming + newline + // data.labelnpm + data.npm
+               //githubing + newline + // data.labelGitHub + data.github
+               linkedining + newline + // data.labelLinkedIn + data.linkedin
+               //webing + newline + newline + // data.labelWeb + data.web
+               carding ; // data.labelCard + data.npx;
+
+//fs.writeFileSync(path.join(__dirname, 'bin/output'), chalk.green(boxen(output, options)));
+fs.writeFileSync(path.join(__dirname, 'bin/output'), chalk.green(output));
